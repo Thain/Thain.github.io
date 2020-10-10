9571def0dd857b2de5d824fb01301a88ebd71ee7
Liam Chung <liam.chung@mail.mcgill.ca>
2020-10-09 23:40:45 -0400

    automated update

---
+++ _site/update.bash
@@ -0,0 +1,11 @@
+cd ../../mymedia/thain.github.io
+for i in $( ls ); do
+	rm -r $i
+done
+cd ../../../websites/ccmp
+for i in $( ls ); do
+	cp -r $i ../../mymedia/thain.github.io
+done
+echo "gem "github-pages", group: :jekyll_plugins" >> ../../mymedia/thain.github.io/Gemfile
+cd ../../mymedia/thain.github.io
+git add .; git commit -m "automated update"; git push --force
